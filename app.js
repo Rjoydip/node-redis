02427f22e3a039f615bcfee3a5efaff4513a63da
Rjoydip <joydipand@gmail.com>
2017-09-30 22:29:36 -0800

    remove(): unwanted package depedency, code

--- app.js
+++ app.js
@@ -1,17 +1,14 @@
 var express = require('express');
 var path = require('path');
-var favicon = require('serve-favicon');
 var logger = require('morgan');
-var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var redis = require('redis');
 
-require("dotenv").config();
-
+// express app instance
 var app = express();
 
 // REDIS
-var client = redis.createClient(); // connected locally
+var client = redis.createClient();
 
 client.on('connect', function () {
   console.log('Redis connected');
@@ -25,15 +22,11 @@ client.on('error', function (error) {
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
   extended: false
 }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', function (req, res) {
   res.render("index.html");
